b627309e4219702b5f422f7635671554f034a25f
Tanguyvans <71881765+Tanguyvans@users.noreply.github.com>
2023-11-12 18:19:03 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 docker-compose -f ./artifacts/channel/create-certificate-with-ca/docker-compose.yaml up -d
 docker-compose -f ./artifacts/docker-compose.yaml up -d
 
-sleep 5
+sleep 30
 ./createChannel.sh
 
 sleep 2
